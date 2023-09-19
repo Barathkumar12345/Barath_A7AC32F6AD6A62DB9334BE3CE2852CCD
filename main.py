d9843c3c7e32a5b65d67077e2e468cc2a53724fb
Barathkumar12345 <144410108+Barathkumar12345@users.noreply.github.com>
2023-09-19 09:45:32 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-year = int(input("enter year to be checked "))
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-if year%4==0:
-  if year%100==0:
-    if year%400==0:
-      print("the year is a leap year!")
-    else:
-      print("the year is not a leap year !")
-  else:
-    print("the year is a leap year!")
-else:
-  print("the year is not a leap year!")
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive value.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        elif amount <= 0:
+            print("Invalid withdrawal amount. Please enter a positive value.")
+        else:
+            print("Insufficient funds for withdrawal.")
+
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: ${self.__account_balance}")
+
+
+# Testing the BankAccount class
+if __name__ == "__main__":
+    account = BankAccount("123456789", "John Doe", 1000)
+
+    account.display_balance()
+    account.deposit(500)
+    account.withdraw(200)
+    account.withdraw(1500)
